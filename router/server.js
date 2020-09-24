c672b7ca0cf0a16badfd9c5b14f18f99e2b61b82
Cazuist <sovrus@mail.ru>
2020-09-24 12:43:15 +0300

    Six commit

--- router/server.js
+++ router/server.js
@@ -14,7 +14,7 @@ app.use(koaBody({
   //multipart: true,
 }));
 
-app.use(async (ctx, next) => {
+/*app.use(async (ctx, next) => {
   const origin = ctx.request.get('Origin');
 
   if (!origin) {
@@ -57,6 +57,65 @@ app.use(async (ctx) => {
   }
 
   ctx.response.body = 'POST';
-});
+});*/
+
+app.use(async (ctx) => {
+  ctx.response.set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': ['DELETE', 'PUT', 'PATCH'],
+  });
+
+  /*if (ctx.request.method === 'GET') {
+    if (ctx.request.querystring === 'tickets') {
+      ctx.response.body = tickets;
+    } else {
+      const id = Number(ctx.request.querystring.slice(9));
+      let ticket = tickets.filter(tick => tick.id == id);
+      ctx.response.body = ticket[0].description;
+    }
+  } else if (ctx.request.method === 'POST') {
+    const ticked = JSON.parse(ctx.request.body);
+    const id = [];
+    let newId = 10;
+    for (const elem of tickets) {
+      id.push(elem.id);
+    }
+    for (let i = 0; i <= id.length; i += 1) {
+      if (id.indexOf(i) === -1) {
+        newId = i;
+        break;
+      }
+    }
+    ticked.id = newId;
+    tickets.push(ticked);
+    ctx.response.body = newId;
+  } else if (ctx.request.method === 'OPTIONS') {
+    ctx.response.body = '';
+  } else if (ctx.request.method === 'DELETE') {
+    const id = Number(ctx.request.querystring.slice(9));
+    tickets = tickets.filter(tick => tick.id != id);
+    ctx.response.body = '';
+  } else if (ctx.request.method === 'PUT') {
+    const id = Number(ctx.request.querystring.slice(9));
+    const nameDescript = JSON.parse(ctx.request.body);
+    for (const elem of tickets) {
+      if (elem.id === id) {
+        elem.name = nameDescript.name;
+        elem.description = nameDescript.description;
+        break;
+      }
+    }
+    ctx.response.body = '';
+  } else if (ctx.request.method === 'PATCH') {
+    const id = ctx.request.body;
+    const ticket = tickets.filter(tick => tick.id == id);
+    if (ticket[0].status) {
+      tickets[tickets.indexOf(ticket[0])].status = false;
+    } else {
+      tickets[tickets.indexOf(ticket[0])].status = true;
+    }*/
+
+    ctx.response.body = ctx.response.method;
+}
 
 const server = http.createServer(app.callback()).listen(port);
